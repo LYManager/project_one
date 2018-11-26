25165c6405f241910983c3258a1e068c969f361a
中链 <zhonglian@zhongliandeMacBook-Pro.local>
2018-11-26 17:10:17 +0800

    添加极光，leancllude 等

---
+++ Lucky/CodeModule/WebViewLoad/BaseTabbarButton.h
@@ -0,0 +1,13 @@
+//
+//  BaseTabbarButton.h
+//  新闻
+//
+//  Created by gyh on 15/9/21.
+//  Copyright © 2015年 apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseTabbarButton : UIButton
+@property (nonatomic , strong) UITabBarItem *item;
+@end
